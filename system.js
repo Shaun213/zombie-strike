2a66fea01327bc3ab9bb59df713dbf501b064b56
Shaun213 <shkasbekar13@gmail.com>
2026-02-25 20:54:09 -0800

    Update system.js

--- system.js
+++ system.js
@@ -1,22 +1,25 @@
 const System = {
-    camera: { x: 0, y: 0, shake: 0 },
+    camera: { x: 0, y: 0, shake: 0, lerp: 0.1 },
     mouse: { x: 0, y: 0, worldX: 0, worldY: 0, down: false },
     keys: {},
+    particles: [],
 
     init() {
         window.addEventListener('keydown', e => this.keys[e.code] = true);
         window.addEventListener('keyup', e => this.keys[e.code] = false);
-        window.addEventListener('mousemove', e => { this.mouse.x = e.clientX; this.mouse.y = e.clientY; });
-        window.addEventListener('mousedown', () => this.mouse.down = true);
-        window.addEventListener('mouseup', () => this.mouse.down = false);
+        window.onmousemove = e => { this.mouse.x = e.clientX; this.mouse.y = e.clientY; };
+        window.onmousedown = () => this.mouse.down = true;
+        window.onmouseup = () => this.mouse.down = false;
     },
 
     update(dt, player, canvas) {
-        // Smooth Glide Camera
-        this.camera.x += (player.x - canvas.width/2 - this.camera.x) * 0.1;
-        this.camera.y += (player.y - canvas.height/2 - this.camera.y) * 0.1;
+        // Camera Glide
+        this.camera.x += (player.x - canvas.width / 2 - this.camera.x) * this.camera.lerp;
+        this.camera.y += (player.y - canvas.height / 2 - this.camera.y) * this.camera.lerp;
+        
         this.mouse.worldX = this.mouse.x + this.camera.x;
         this.mouse.worldY = this.mouse.y + this.camera.y;
-        if(this.camera.shake > 0) this.camera.shake *= 0.9;
+
+        if (this.camera.shake > 0) this.camera.shake *= 0.9;
     }
 };
