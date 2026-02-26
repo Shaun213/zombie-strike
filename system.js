3da2bb38a63867e6e05b6dadf0b1e2256958e326
Shaun213 <shkasbekar13@gmail.com>
2026-02-25 20:43:41 -0800

    Update system.js

--- system.js
+++ system.js
@@ -1,40 +1,41 @@
 const System = {
+    camera: { x: 0, y: 0, zoom: 1, shake: 0 },
+    keys: {},
+    mouse: { x: 0, y: 0, down: false },
     particles: [],
-    fps: 0,
-    lastTime: 0,
-    shake: 0,
-
+    
     init() {
-        this.lastTime = performance.now();
+        window.addEventListener('keydown', e => this.keys[e.code] = true);
+        window.addEventListener('keyup', e => this.keys[e.code] = false);
+        window.addEventListener('mousemove', e => {
+            this.mouse.x = e.clientX;
+            this.mouse.y = e.clientY;
+        });
+        window.addEventListener('mousedown', () => this.mouse.down = true);
+        window.addEventListener('mouseup', () => this.mouse.down = false);
     },
 
-    update(currentTime) {
-        const dt = (currentTime - this.lastTime) / 1000;
-        this.lastTime = currentTime;
-        this.fps = Math.round(1 / dt);
-        
-        // Particle Physics
-        for (let i = this.particles.length - 1; i >= 0; i--) {
-            let p = this.particles[i];
-            p.x += p.vx; p.y += p.vy;
-            p.life -= 0.02;
-            if (p.life <= 0) this.particles.splice(i, 1);
-        }
-        
+    // The "Juice": Camera shake and Blood
+    update(dt, target) {
+        // Camera smooth follow
+        this.camera.x += (target.x - this.camera.x) * 0.1;
+        this.camera.y += (target.y - this.camera.y) * 0.1;
         if (this.shake > 0) this.shake *= 0.9;
-        return dt;
+
+        // Particle physics
+        this.particles.forEach((p, i) => {
+            p.x += p.vx; p.y += p.vy;
+            p.life -= dt * 2;
+            if (p.life <= 0) this.particles.splice(i, 1);
+        });
     },
 
-    spawnBlood(x, y, count = 5) {
-        for (let i = 0; i < count; i++) {
+    spawnBlood(x, y) {
+        for(let i=0; i<8; i++) {
             this.particles.push({
-                x, y, vx: (Math.random() - 0.5) * 6, vy: (Math.random() - 0.5) * 6,
-                life: 1.0, color: '#8b0000', size: Math.random() * 4
+                x, y, vx: (Math.random()-0.5)*10, vy: (Math.random()-0.5)*10,
+                life: 1, color: '#ff3e3e'
             });
         }
-    },
-
-    dist(x1, y1, x2, y2) {
-        return Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
     }
 };
